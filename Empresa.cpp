16840876c5f1670596ec5f6e1f54595c5fa15d57
brauliojmz <brauliojmz@gmail.com>
2024-11-11 16:44:01 -0600

    Elaboracion de clase Empresa

---
+++ Empresa.cpp
@@ -0,0 +1,5 @@
+//
+// Created by braul on 11/11/2024.
+//
+
+#include "Empresa.h"
---
+++ Empresa.h
@@ -0,0 +1,13 @@
+#ifndef EMPRESA_H
+#define EMPRESA_H
+
+#include <iostream>
+using namespace std;
+
+class Empresa {
+private:
+
+public:
+};
+
+#endif //EMPRESA_H

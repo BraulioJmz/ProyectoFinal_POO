52362ed3db0ddfc8ea25e04c2912f08a7cd96391
brauliojmz <brauliojmz@gmail.com>
2024-11-17 20:15:44 -0600

    Clase Programador completa Agregación de la base para menú

---
+++ cmake-build-debug/CMakeFiles/ProyectoFinal.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProyectoFinal.dir/Disenador.cpp.obj"
+  "CMakeFiles/ProyectoFinal.dir/Disenador.cpp.obj.d"
+  "CMakeFiles/ProyectoFinal.dir/Empresa.cpp.obj"
+  "CMakeFiles/ProyectoFinal.dir/Empresa.cpp.obj.d"
+  "CMakeFiles/ProyectoFinal.dir/Programador.cpp.obj"
+  "CMakeFiles/ProyectoFinal.dir/Programador.cpp.obj.d"
+  "CMakeFiles/ProyectoFinal.dir/Proyecto.cpp.obj"
+  "CMakeFiles/ProyectoFinal.dir/Proyecto.cpp.obj.d"
+  "CMakeFiles/ProyectoFinal.dir/Trabajador.cpp.obj"
+  "CMakeFiles/ProyectoFinal.dir/Trabajador.cpp.obj.d"
+  "CMakeFiles/ProyectoFinal.dir/main.cpp.obj"
+  "CMakeFiles/ProyectoFinal.dir/main.cpp.obj.d"
+  "ProyectoFinal.exe"
+  "ProyectoFinal.exe.manifest"
+  "ProyectoFinal.pdb"
+  "libProyectoFinal.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProyectoFinal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
